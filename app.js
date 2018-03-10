d298d4515705a062d4bddb7b43701573197af391
Michael Hellein <themichaek@gmail.com>
2018-03-10 14:30:19 -0500

    Different buttons, total counts displayed.

--- app.js
+++ app.js
@@ -1,41 +1,87 @@
-var current = 'nothing';
-var queued = 0;
-var inProgress = false;
+var speaking = false;
+var voices = [];
 
-function say(message, done) {
+function populateVoiceList() {
+  if(typeof speechSynthesis === 'undefined') {
+    return;
+  }
+
+  voices = speechSynthesis.getVoices();
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function say(message, voiceName, done) {
+	voiceName = voiceName || 'Alex';
 	var msg = new SpeechSynthesisUtterance(message);
-	inProgress = true;
+	msg.voice = voices.filter(function(voice) { return voice.name === voiceName; })[0];
+
+	speaking = true;
 	msg.onend = function(){
-		inProgress = false;
+		speaking = false;
 		done && done();
 	};
-    window.speechSynthesis.speak(msg);
+	window.speechSynthesis.speak(msg);
 }
 
-function giveMe(implement) {
-	return function(){
-		say('I will give you a '+implement+'!!');
-		current = implement;
-	};
+function giveMe(implement, count, voiceName, done) {
+	var verb = count < 0 ? 'take away' : 'give you';
+	var number = Math.abs(count);
+	var plural = number !== 1;
+	say('I will ' + verb + ' ' + number + ' ' + current.implement + (plural ? 's' : ''), voiceName, done);
 }
 
-document.querySelector('.circle').addEventListener('click', giveMe('spoon'));
-document.querySelector('.square').addEventListener('click', giveMe('plate'));
-document.querySelector('.triangle').addEventListener('click', giveMe('fork'));
+var actions = [
+	{ implement: 'nothing', voice: 'Alex' },
+	{ implement: 'coffee', icon: '\u2615', voice: 'Hysterical', button: '.circle' },
+	{ implement: 'knife', icon: String.fromCodePoint(0x1F52A), voice: 'Moira', button: '.square' },
+	{ implement: 'spoon', icon: String.fromCodePoint(0x1F944), voice: 'Zarvox', button: '.circle-table' },
+	{ implement: 'cyclone', icon: String.fromCodePoint(0x1F300), voice: 'Deranged', button: '.triangle' },
+];
+
+var current = actions[0];
+
+actions.forEach(action => {
+	var countDisplay;
+
+	action.total = 0;
+	action.queued = 0;
+
+	action.giveMe = (count, done) => {
+		action.total += count;
+		if (countDisplay) {
+			countDisplay.innerText = action.total;
+		}
+		giveMe(action.implement, count, action.voice, () => {
+			if (action.queued) {
+				action.giveMe(action.queued);
+				action.queued = 0;
+			}
+		});
+	}
+
+	if (action.button) {
+		var button = document.querySelector(action.button);
+		if (action.icon) {
+			button.innerHTML = action.icon;
+		}
+		countDisplay = button.nextSibling;
+		button.addEventListener('click', () => {
+			current = action;
+			action.giveMe(1);
+		});
+	}
+});
 
 document.body.addEventListener('keypress', function(event) {
 	var count = parseInt(event.charCode, 10) - 48;
 	var verb;
-	if (!inProgress) {
-		verb = count < 0 ? 'take away' : 'give you';
-		say('I will ' + verb + ' ' + Math.abs(count) + ' ' + current + (Math.abs(count) > 1 ? 's' : ''), function(){
-			if (queued) {
-				verb = queued < 0 ? 'take away' : 'give you';
-				say('I will ' + verb + ' ' + Math.abs(queued) + ' ' + current + 's' );
-				queued = 0;
-			}
-		});
+	if (!speaking) {
+		current.giveMe(count);
 	} else {
-		queued += count;
+		current.queued += count;
 	}
 });
